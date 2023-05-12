fccbd6713e0c3fa408a6c4314967c5b3584b4cf4
majuzi <2318332846@qq.com>
2023-05-12 10:21:57 +0800

    提交部分在线咨询页面

--- src/pages/online-consult/online-consult.vue
+++ src/pages/online-consult/online-consult.vue
@@ -1,7 +1,224 @@
 <template>
-  <view>online</view>
+  <view>
+    <scroll-view class="chat" scroll-with-animation="true" scroll-y="true">
+      <view class="chat-main">
+        <view v-for="(item, index) in msgs" :key="index" class="chat-ls">
+          <view class="chat-time">{{ item.time }}</view>
+          <view v-if="item.id != 'b'" class="msg-m msg-left">
+            <image :src="item.imgurl" class="user-img"></image>
+            <view v-if="item.types == 0" class="message">
+              <view class="msg-text">{{ item.message }}</view>
+            </view>
+            <view v-if="item.types == 1" class="message">
+              <image
+                :src="item.message"
+                class="msg-img"
+                mode="widthFix"
+              ></image>
+            </view>
+          </view>
+          <view v-if="item.id != 'a'" class="msg-m msg-right">
+            <image :src="item.imgurl" class="user-img"></image>
+            <view v-if="item.types == 0" class="message">
+              <view class="msg-text">{{ item.message }}</view>
+            </view>
+            <view v-if="item.types == 1" class="message">
+              <image
+                :src="item.message"
+                class="msg-img"
+                mode="widthFix"
+              ></image>
+            </view>
+          </view>
+        </view>
+      </view>
+    </scroll-view>
+  </view>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import { onLoad } from "@dcloudio/uni-app";
+import { reactive } from "vue";
 
-<style lang="scss" scoped></style>
+let msgs = reactive<
+  {
+    id: string;
+    imgurl: string;
+    message: string;
+    types: number;
+    time: Date;
+    tip: number;
+  }[]
+>([]);
+onLoad(() => {
+  getMsg();
+  console.log(msgs);
+});
+
+//获取聊天数据
+function getMsg() {
+  let msg = [
+    {
+      id: "b",
+      imgurl:
+        "https://mp-4dc08b2f-eb0d-40fc-8b5f-e5ab2e09218f.cdn.bspapp.com/cloudstorage/8125c34d-f5cb-448b-b47b-21d72c7044b5.jpg",
+      message: "的手机待机",
+      types: 0,
+      time: new Date(1626244869999),
+      tip: 1
+    },
+    {
+      id: "a",
+      imgurl:
+        "https://mp-4dc08b2f-eb0d-40fc-8b5f-e5ab2e09218f.cdn.bspapp.com/cloudstorage/a3b7b174-b05b-426c-b492-406cdfa93388.jpg",
+      message:
+        "那边就不是绝对不接受多年时间倒计时倒计时阶段吧年的世界杯的建设北京的三百",
+      types: 0,
+      time: new Date(1626244869789),
+      tip: 0
+    },
+    {
+      id: "b",
+      imgurl:
+        "https://mp-4dc08b2f-eb0d-40fc-8b5f-e5ab2e09218f.cdn.bspapp.com/cloudstorage/8125c34d-f5cb-448b-b47b-21d72c7044b5.jpg",
+      message:
+        "干撒干撒干啥干啥被杀uu时的速度多少都会还是仅仅是当今时代简单觉得你",
+      types: 0,
+      time: new Date(1626244865437),
+      tip: 1
+    },
+    {
+      id: "a",
+      imgurl:
+        "https://mp-4dc08b2f-eb0d-40fc-8b5f-e5ab2e09218f.cdn.bspapp.com/cloudstorage/8125c34d-f5cb-448b-b47b-21d72c7044b5.jpg",
+      message:
+        "https://mp-4dc08b2f-eb0d-40fc-8b5f-e5ab2e09218f.cdn.bspapp.com/cloudstorage/88116a40-c1bc-42e3-8d7f-1c3e66fd06d8.jpg",
+      types: 1,
+      time: new Date(1626244868250),
+      tip: 1
+    },
+    {
+      id: "a",
+      imgurl:
+        "https://mp-4dc08b2f-eb0d-40fc-8b5f-e5ab2e09218f.cdn.bspapp.com/cloudstorage/a3b7b174-b05b-426c-b492-406cdfa93388.jpg",
+      message:
+        "和倒计时倒计时的术后读书的的不俗百度搜不到下班时间对不上不得好死年的世界杯的",
+      types: 0,
+      time: new Date(1626244866838),
+      tip: 1
+    },
+    {
+      id: "b",
+      imgurl:
+        "https://mp-4dc08b2f-eb0d-40fc-8b5f-e5ab2e09218f.cdn.bspapp.com/cloudstorage/8125c34d-f5cb-448b-b47b-21d72c7044b5.jpg",
+      message:
+        "https://mp-4dc08b2f-eb0d-40fc-8b5f-e5ab2e09218f.cdn.bspapp.com/cloudstorage/2eca5681-d17f-44f3-965c-1ba6e43fb50a.jpg",
+      types: 1,
+      time: new Date(1626244868250),
+      tip: 1
+    },
+    {
+      id: "a",
+      imgurl:
+        "https://mp-4dc08b2f-eb0d-40fc-8b5f-e5ab2e09218f.cdn.bspapp.com/cloudstorage/a3b7b174-b05b-426c-b492-406cdfa93388.jpg",
+      message:
+        "的手机待机时间的电脑手机电脑是加拿大电脑上的几年时间你手机电脑手机电脑的难度",
+      types: 0,
+      time: new Date(1626244868888),
+      tip: 1
+    },
+
+    {
+      id: "b",
+      imgurl:
+        "https://mp-4dc08b2f-eb0d-40fc-8b5f-e5ab2e09218f.cdn.bspapp.com/cloudstorage/8125c34d-f5cb-448b-b47b-21d72c7044b5.jpg",
+      message:
+        "的失败的话说不定会被北师大版技术大版电脑手机难道就是当年那年暑假绝对加速度难道就是你的就是你的",
+      types: 0,
+      time: new Date(1626244868250),
+      tip: 1
+    }
+  ];
+  for (let i = 0; i < msg.length; i++) {
+    msgs.unshift(msg[i]);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.chat {
+  height: 100%;
+
+  .chat-main {
+    padding: 100rpx 32rpx 120rpx;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .chat-ls {
+    .chat-time {
+      font-size: 24rpx;
+      color: rgba(39, 40, 50, 0.3);
+      line-height: 34rpx;
+      padding: 20rpx 0;
+      text-align: center;
+    }
+
+    .msg-m {
+      display: flex;
+      padding: 20rpx 0;
+
+      .user-img {
+        flex: none;
+        width: 100rpx;
+        height: 100rpx;
+        border-radius: 10rpx;
+      }
+
+      .message {
+        flex: none;
+        max-width: 480rpx;
+      }
+
+      .msg-text {
+        font-size: 35rpx;
+        line-height: 44rpx;
+        padding: 15rpx 24rpx;
+      }
+
+      .msg-img {
+        max-width: 400rpx;
+        border-radius: 20rpx;
+      }
+    }
+
+    .msg-left {
+      flex-direction: row;
+
+      .msg-text {
+        margin-left: 16rpx;
+        background-color: #fff;
+        border-radius: 0rpx 20rpx 20rpx 20rpx;
+      }
+
+      .msg-img {
+        margin-left: 16rpx;
+      }
+    }
+
+    .msg-right {
+      flex-direction: row-reverse;
+
+      .msg-text {
+        margin-right: 16rpx;
+        background-color: rgba(255, 228, 49, 0.8);
+        border-radius: 0rpx 20rpx 20rpx 20rpx;
+      }
+
+      .msg-img {
+        margin-right: 16rpx;
+      }
+    }
+  }
+}
+</style>
